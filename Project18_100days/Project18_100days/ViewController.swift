24b0ee9f691dde6f7c32afd9aa09944b80d39d44
Aleksandr Zubov <alexanderzubov@icloud.com>
2023-02-22 18:42:31 +0100

    Project18_tutorial

--- Project18_100days/Project18_100days/ViewController.swift
+++ Project18_100days/Project18_100days/ViewController.swift
@@ -11,7 +11,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("I'm inside the viewDidLoad() method!")
+        print(1, 2, 3, 4, 5, separator: "-")
+        print("Some message", terminator: "")
+        
+        assert(1 == 1, "Maths failure!")
+        //assert(1 == 2, "Maths failure!")
+        
+        //assert(myReallySlowMethod() == true, "The slow method returned false, which is a bad thing!")
+        
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
     }
 
 
